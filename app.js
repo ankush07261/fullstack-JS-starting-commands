636c097f8d0c2c57a964cced0eb4a7c1e776ecbf
Ankush Hegde <59443849+ankush07261@users.noreply.github.com>
2023-08-31 09:31:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 //jshint esversion:6
+//MONGODB
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-
 //connection url
 const url = 'mongodb://localhost:27017';
-
 //database name
 const dbName = 'name';
-
 //create a new mongoclient
 const client = new MongoClient(url);
-
 //use connect method to connect to the server
 client.connect(function (err) {
     assert.equal(null, err);
     console.log("connected successfully to server");
-
     const db = client.db(dbName);
-
     client.close();
+});
+
+//ExpressJS, EJS, body-parser, lodash
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const _ = require("lodash");
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.render('pageName',{});
+})
+
+
+app.post('/compose', (req, res) => {
+  //title = req.body.title;
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
