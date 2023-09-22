55a76aca68d76dbce3907bc689771c748f3f696f
Ankush Hegde <59443849+ankush07261@users.noreply.github.com>
2023-09-22 17:36:09 +0530

    Added particular comments in app.js

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 //for MongoDB & MONGOOSE refer script.js and user.js
 
 //ExpressJS, EJS, body-parser, lodash
+
+//importing modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const _ = require("lodash");
 
+//declarations
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//methods
 app.get('/', (req, res) => {
   //res.render('pageName',{});
 })
